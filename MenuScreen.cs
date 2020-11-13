c744f6e9a99c06b49fed10bc4fa66b11518f85c3
Yulia Groshenko <yulyagroshenko@mail.ru>
2020-11-14 04:13:30 +0500

    небольшие изменения

--- MenuScreen.cs
+++ MenuScreen.cs
@@ -6,30 +6,39 @@ namespace Filwords.Menu
 {
     class MenuScreen
     {
-        public static void Print()
+       
+        public static void PrintMenu(ConsoleColor colorNewGame, ConsoleColor colorResume, ConsoleColor colorRating, ConsoleColor colorExit)
         {
             string name = "F I L L W O R D S";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.SetCursorPosition((Console.WindowWidth / 2) - (Console.WindowHeight / 2), 1);
+            Console.WriteLine($"{name}");
+            Console.ResetColor();
+
             string newGame = "New Game";
             string resume = "Resume";
             string rating = "Rating";
             string exit = "Exit";
+            
 
-            Console.ForegroundColor = ConsoleColor.Cyan;
-
-            Console.SetCursorPosition((Console.WindowWidth / 2) - (Console.WindowHeight / 2), 1); 
-            Console.WriteLine($"{name}");
-            Console.ResetColor();
             Console.SetCursorPosition((Console.WindowWidth / 2) - (Console.WindowHeight / 2) + 5, 2);
+            Console.ForegroundColor = colorNewGame;
             Console.WriteLine($"{newGame}");
 
             Console.SetCursorPosition((Console.WindowWidth / 2) - (Console.WindowHeight / 2) + 6, 3);
+            Console.ForegroundColor = colorResume;
             Console.WriteLine($"{resume}");
 
             Console.SetCursorPosition((Console.WindowWidth / 2) - (Console.WindowHeight / 2) + 6, 4);
+            Console.ForegroundColor = colorRating;
             Console.WriteLine($"{rating}");
 
             Console.SetCursorPosition((Console.WindowWidth / 2) - (Console.WindowHeight / 2) + 7, 5);
+            Console.ForegroundColor = colorExit;
             Console.WriteLine($"{exit}");
+ 
+
         }
+        
     }
 }
